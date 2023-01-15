6eb90e723ad7ec2a09e53f67b69549f02fb752cb
Andrew Zaiko <andrewzaiko@gmail.com>
2023-01-15 22:24:01 +0300

    Создал валидатор 'check_ad_name_length'

--- advertisements/validators.py
+++ advertisements/validators.py
@@ -6,4 +6,12 @@ def check_ad_is_published(value: bool):
     Проверяет значение поля 'is_published' модели 'Advertisement'
     """
     if value:
-        raise ValidationError("Значение не может быть 'True'")
+        raise ValidationError(" Поле 'is_published' не может быть 'True' ")
+
+
+def check_ad_name_length(value: str):
+    """
+    Проверяет минимальную длину поля 'name' модели 'Advertisement'
+    """
+    if len(value) < 10:
+        raise ValidationError(" Поле 'name' не может быть короче 10 символов ")
